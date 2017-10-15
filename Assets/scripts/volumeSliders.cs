8635d84524e5b8d69242875f29d0d2d4c34fa341
Andrew <shimeroh@gmail.com>
2017-10-15 21:05:52 +1300

    Hooked up the music slider to the overall volume of the game

---
+++ Assets/scripts/volumeSliders.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volumeSliders : MonoBehaviour {
+
+    [SerializeField]
+    Slider volumeSlider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AudioListener.volume = volumeSlider.value;
+	}
+}
