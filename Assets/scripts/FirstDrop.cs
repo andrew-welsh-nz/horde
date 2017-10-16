0bc3fd1ab4fb32979704f7a0e117a4b22512923a
Andrew <shimeroh@gmail.com>
2017-10-17 11:35:50 +1300

    The drop at the beginning has been added, and must be tapped for the game to begin

---
+++ Assets/scripts/FirstDrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstDrop : MonoBehaviour {
+
+    [SerializeField]
+    shooting game;
+
+    [SerializeField]
+    ActiveOnPlay firstEnemy;
+
+	public void StartGame()
+    {
+        game.StartGame();
+        firstEnemy.StartGame();
+    }
+}
